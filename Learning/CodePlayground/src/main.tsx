c8468c3f2e3dece9bc496beeabbb467816116cea
Curtis Esau <curtisesau4@gmail.com>
2025-03-12 22:35:32 -0700

    style: add benefits section

--- Learning/CodePlayground/src/main.tsx
+++ Learning/CodePlayground/src/main.tsx
@@ -4,12 +4,14 @@ import './index.css'
 import Nav from './nav'
 import Heading from './sections/heading/heading'
 import Products from './sections/products/products'
+import Benefits from './sections/benefits/benefits'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Nav />
     <Heading />
     <Products />
+    <Benefits />
     <div id="bluebox"></div>
   </StrictMode>,
 )
---
+++ Learning/CodePlayground/src/sections/benefits/benefits.tsx
@@ -0,0 +1,37 @@
+import './benefits.css';
+import Button from '../../util/button/button';
+
+const features = [{
+    title: "Eco-sustainable:",
+    description: "All recycled materials, 0% CO2 emissions."
+}, {
+    title: "Hyphoallergenic:",
+    description: "!00% natural, human friendly ingredients."
+}, {
+    title: "Handmade:",
+    description: "All candles are craftly made with love."
+}, {
+    title: "Long burning:",
+    description: "No more waste. Created for last long."
+}];
+
+const listItems = features.map(feature => 
+    <p className="feature__item">✓ <b>{feature.title}</b> {feature.description}</p>
+);
+
+export default function Benefits() {
+  return (
+    <div className="benefits">
+      <div className="benefits__text-left">
+        <h2>Clean and fragrant soy wax</h2>
+        <p className="benefits__subtitle">Made for your home and for your wellness</p>
+        {listItems}
+        <Button
+          id="benefits__button"
+          content="Learn more"
+        />
+      </div>
+      <div className="benefits__image"><img src="src/assets/images/candles-two.png" alt="two candles" /></div>
+    </div>
+  );
+}
